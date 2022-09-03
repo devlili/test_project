35abc07441270e5932338fba9882a3204509e43d
devlili <lilkin_park@mail.ru>
2022-09-03 21:51:42 +0500

    add new class

--- pages/locators.py
+++ pages/locators.py
@@ -8,4 +8,11 @@ class LoginPageLocators():
     LOGIN_FORM = (By.ID, "login_form")
     REGISTER_FORM = (By.ID, "register_form")
 
+class ProductPageLocators():
+    ADD_BASKET = (By.CSS_SELECTOR, "button.btn.btn-lg.btn-primary.btn-add-to-basket")
+    NAME_BOOK = (By.CSS_SELECTOR, "div.product_main h1")
+    ADD_BOOK = (By.CSS_SELECTOR, "div.alertinner strong")
+    PRICE = (By.CSS_SELECTOR, "p.price_color")
+    PRICE_BASKET = (By.CSS_SELECTOR, "div.alertinner p strong")
+
 
