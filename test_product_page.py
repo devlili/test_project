20aeb9ddb62b5fd46577bb6dcf9475015e02ebc8
devlili <lilkin_park@mail.ru>
2022-09-03 21:58:51 +0500

    add new file with test case

---
+++ test_product_page.py
@@ -0,0 +1,9 @@
+from .pages.product_page import ProductPage
+
+def test_guest_can_add_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_basket()
+    page.book_name()
+    page.price()
