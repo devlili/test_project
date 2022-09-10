9e93c5f7b55b7102c58da6789b4a0df5acc9842d
devlili <lilkin_park@mail.ru>
2022-09-10 16:01:37 +0500

    add 3 tests

--- test_product_page.py
+++ test_product_page.py
@@ -1,11 +1,24 @@
 import pytest
 from .pages.product_page import ProductPage
 
-@pytest.mark.parametrize('promo_offer', ["0","1", "3", "4", "5", "6", pytest.param ("7", marks=pytest.mark.xfail(reason="some bug")), "8", "9"])
-def test_guest_can_add_product_to_basket(browser, promo_offer):
-    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo_offer}"
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.go_to_basket()
-    page.book_name()
-    page.price()
+    page.should_not_be_success_message()
+
+def test_guest_cant_see_success_message(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_not_be_success_message()
+
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_basket()
+    page.success_message_should_disappear()
+
+
