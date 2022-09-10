a47e3f1d454356597c685b2058f919e789d79a6a
devlili <lilkin_park@mail.ru>
2022-09-10 15:59:32 +0500

    add new locator

--- pages/locators.py
+++ pages/locators.py
@@ -14,5 +14,7 @@ class ProductPageLocators():
     ADD_BOOK = (By.CSS_SELECTOR, "div.alertinner strong")
     PRICE = (By.CSS_SELECTOR, "p.price_color")
     PRICE_BASKET = (By.CSS_SELECTOR, "div.alertinner p strong")
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, "#messages div:nth-child(1)")
+
 
 
