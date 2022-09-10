14a8e2283f54c618fe380e25135e465943211114
devlili <lilkin_park@mail.ru>
2022-09-10 19:21:58 +0500

    add 2 deff

--- pages/base_page.py
+++ pages/base_page.py
@@ -4,6 +4,7 @@ import math
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.common.exceptions import TimeoutException
+from .locators import BasePageLocators
 
 
 class BasePage():
@@ -11,6 +12,13 @@ class BasePage():
         self.browser = browser
         self.url = url
         #self.browser.implicitly_wait(timeout)
+
+    def go_to_login_page(self):
+        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
 
     def open(self):
         self.browser.get(self.url)
