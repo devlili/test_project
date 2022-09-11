765d1540b63f71c9c562585046d1405b7734ce87
devlili <lilkin_park@mail.ru>
2022-09-11 23:18:45 +0500

    add new locators

--- pages/locators.py
+++ pages/locators.py
@@ -3,6 +3,10 @@ from selenium.webdriver.common.by import By
 class LoginPageLocators():
     LOGIN_FORM = (By.ID, "login_form")
     REGISTER_FORM = (By.ID, "register_form")
+    EMAIL = (By.NAME, "registration-email")
+    PASSWORD = (By.NAME, "registration-password1")
+    PASSWORD_CONFIRM = (By.NAME, "registration-password2")
+    LOG_IN = (By.NAME, "registration_submit")
 
 class ProductPageLocators():
     ADD_BASKET = (By.CSS_SELECTOR, "button.btn.btn-lg.btn-primary.btn-add-to-basket")
@@ -15,6 +19,7 @@ class ProductPageLocators():
 class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     GO_BASKET = (By.XPATH, '//*[@id="default"]/header/div[1]/div/div[2]/span/a')
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
 
 class Basket():
     BASKET_CLEAR = (By.CSS_SELECTOR, "basket-items")
