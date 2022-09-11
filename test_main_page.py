ca0f66646505f7161fb82ceb37905cfe41a83027
devlili <lilkin_park@mail.ru>
2022-09-11 13:27:41 +0500

    add new test

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,7 @@
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
+from .pages.basket_page import BasketPage
+from .pages.base_page import BasePage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com"
@@ -14,3 +16,12 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_basket_page()
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.should_not_be_goods_in_basket()
+    basket_page.basket_is_empty_message()
