52058e2b07fe55948f85e4a7478a6c5e5c5c79b4
devlili <lilkin_park@mail.ru>
2022-09-11 13:27:23 +0500

    rename def

--- pages/product_page.py
+++ pages/product_page.py
@@ -2,7 +2,7 @@ from .base_page import BasePage
 from .locators import ProductPageLocators
 
 class ProductPage(BasePage):
-    def go_to_basket(self):
+    def add_to_basket(self):
         basket = self.browser.find_element(*ProductPageLocators.ADD_BASKET)
         basket.click()
         #self.solve_quiz_and_get_code()
